9af9247b724733ae1be59e111231f26e4b6af893
bfforsythe <bfforsythe@alaska.edu>
2022-11-01 14:09:05 -0800

    added asset folder

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,17 +15,11 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
     return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
-        ),
-        body:  Center(
-          child: Text(wordPair.asPascalCase),
-        ),
-      ),
+      const Image(
+        image: NetworkImage()
+      )
+
     );
   }
 }
