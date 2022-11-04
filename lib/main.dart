d4325467802b086a9fa234b62ebae72607e1dc43
bfforsythe <bfforsythe@alaska.edu>
2022-11-04 12:59:29 -0800

    added logo and button functionality

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@
 
 import 'package:flutter/material.dart';
 
-
 void main() {
   runApp(const MyApp());
 }
@@ -14,14 +13,11 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
-      title: 'ChittR',
-
+    return MaterialApp(
       home: MyHomePage(),
     );
   }
 }
-
 
 // Stateful widget for __myHomePage()
 
@@ -30,33 +26,64 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage>{
+class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-    appBar: AppBar(
-      title:Text("Image Test"),
-      backgroundColor:Colors.brown,
-    ),
-
-
-    body: Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: ListView(
-        children:<Widget>[
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-
-          child: Image.asset('assets/images/lowercampus.png',
-                              width: 200,
-                              height: 500,
-                              fit: BoxFit.contain,
-                              semanticLabel: 'UAF Lower Campus Map',
-          ),
-          )
-        ]
+      appBar: AppBar(
+        title: Text("ChittR (Lower Campus)"),
+        centerTitle: (true),
+        backgroundColor: Colors.brown,
       ),
-    ),
+      body: Center(
+        child: Stack(
+          children: [
+            Image.asset(
+              'assets/images/lowercampus.png',
+              width: 400,
+              height: 500,
+              fit: BoxFit.contain,
+              semanticLabel: 'UAF Lower Campus Map',
+            ),
+
+            Align( // Brooks Building
+              alignment: FractionalOffset(0.5, 0.04),
+              child: IconButton(
+                icon: Image.asset('assets/images/brookspin.png'),
+                iconSize: 78,
+                onPressed: () {}, // TODO, ADD CLICK FUNCTIONALITY
+              ),
+            ),
+
+            Align( // Bunnell Building
+              alignment: FractionalOffset(0.35, 0.5),
+              child: IconButton(
+                icon: Image.asset('assets/images/bunnellpin.png'),
+                iconSize: 78,
+                onPressed: () {}, // TODO, ADD CLICK FUNCTIONALITY
+              ),
+            ),
+
+            Align( // Duckering Building
+              alignment: FractionalOffset(0.75, 0.15),
+              child: IconButton(
+                icon: Image.asset('assets/images/duckpin.png'),
+                iconSize: 78,
+                onPressed: () {}, // TODO, ADD CLICK FUNCTIONALITY
+              ),
+            ),
+
+            Align( // ELIF Building
+              alignment: FractionalOffset(0.87, 0.58),
+              child: IconButton(
+                icon: Image.asset('assets/images/elifpin.png'),
+                iconSize: 78,
+                onPressed: () {}, // TODO, ADD CLICK FUNCTIONALITY
+              ),
+            ),
+          ], // CHILDREN
+        ),
+      ),
     );
   }
 }
