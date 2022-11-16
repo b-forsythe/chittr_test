717ac67ad2aadc792270eb7cbbb7adb59c34873a
bfforsythe <bfforsythe@alaska.edu>
2022-11-16 13:18:05 -0900

    DROPDOWN WORKS

--- lib/main.dart
+++ lib/main.dart
@@ -1,104 +1,106 @@
-// Copyright 2018 The Flutter team. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+// Copyright 2022 ChittR LLC.
 
+import 'package:chittr_test/main.dart';
 import 'package:flutter/material.dart';
 
-void main() { // Base run for App.
+void main() {
+  // Base run for App.
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget { // Base app creation
+class MyApp extends StatelessWidget {
+  // Base app creation
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: MyHomePage(),
+      initialRoute: "/home",
+      routes: {
+        "/home": (context) => MyHomePage(),
+        '/brooks': (context) => BrooksPage(),
+        '/bunnell': (context) => BunnellPage(),
+        '/duck': (context) => DuckPage(),
+        '/elif': (context) => ELIFPage(),
+      },
     );
   }
 }
 
-
-class MyHomePage extends StatefulWidget { // Stateful widget for MyHomePage()
+class MyHomePage extends StatefulWidget {
+  // Stateful widget for MyHomePage()
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {    // Creation of Home Page
+class _MyHomePageState extends State<MyHomePage> {
+  // Creation of Home Page
   @override
-  Widget build(BuildContext context) {    // Design Widget
+  Widget build(BuildContext context) {
+    // Design Widget
     return Scaffold(
-      appBar: AppBar(     // "Header" for Main page
+      appBar: AppBar(
+        // "Header" for Main page
         title: Text("ChittR (Lower Campus)"),
         centerTitle: (true),
         backgroundColor: Colors.brown,
       ),
-      body: Center(     // <- This constitutes everything under the header
+      body: Center(
+        // <- This constitutes everything under the header
         child: Stack(
           children: [
-            Image.asset(    // Image Creator for lower campus map
+            Image.asset(
+              // Image Creator for lower campus map
               'assets/images/lowercampus.png',
               width: 400,
               height: 500,
               fit: BoxFit.contain,
               semanticLabel: 'UAF Lower Campus Map',
             ),
-
-            Align(    // Brooks Building
-              alignment: const FractionalOffset(0.5, 0.04),     // Determines x,y position of button
+            Align(
+              // Brooks Building
+              alignment: const FractionalOffset(0.5, 0.04),
+              // Determines x,y position of button
               child: IconButton(
-                icon: Image.asset('assets/images/brookspin.png'),     // Loads image
-                iconSize: 78,     // Sets size
+                icon: Image.asset('assets/images/brookspin.png'), // Loads image
+                iconSize: 78, // Sets size
                 onPressed: () {
-                  Navigator.push(     // Push adds a new "Route" so that app can move to that screen
-                    context,
-                    MaterialPageRoute(builder: (context) => const BrooksPage()), // On click, will move to the -- const (pageName) page.
-                  );
+                  Navigator.pushNamed(context, '/brooks');
                 },
-
               ),
             ),
-
-            Align( // Bunnell Building  ( See Brooks building comments for how all this works)
+            Align(
+              // Bunnell Building  ( See Brooks building comments for how all this works)
               alignment: const FractionalOffset(0.35, 0.5),
               child: IconButton(
                 icon: Image.asset('assets/images/bunnellpin.png'),
                 iconSize: 78,
                 onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => const BunnellPage()),
-                  );
+                  Navigator.pushNamed(context, '/bunnell');
                 },
               ),
             ),
-
-            Align( // Duckering Building
+            Align(
+              // Duckering Building
               alignment: const FractionalOffset(0.75, 0.15),
               child: IconButton(
                 icon: Image.asset('assets/images/duckpin.png'),
                 iconSize: 78,
                 onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => const DuckPage()),
-                  );
+                  Navigator.pushNamed(context, '/duck');
                 },
               ),
             ),
-
-            Align( // ELIF Building
+            Align(
+              // ELIF Building
               alignment: const FractionalOffset(0.87, 0.58),
               child: IconButton(
                 icon: Image.asset('assets/images/elifpin.png'),
                 iconSize: 78,
                 onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => ELIFPage()),
-                  );
+                  Navigator.pushNamed(context, '/elif');
                 },
               ),
             ),
@@ -113,23 +115,23 @@ class _MyHomePageState extends State<MyHomePage> {    // Creation of Home Page
 //             Brooks Building
 //    -------------------------------
 
-class BrooksPage extends StatelessWidget {    // Creates a new "Page" using a class.
+class BrooksPage extends StatelessWidget {
+  // Creates a new "Page" using a class.
   const BrooksPage({super.key});
 
   @override
-
-  Widget build(BuildContext context){   // Build Widget for design of page.
+  Widget build(BuildContext context) {
+    // Build Widget for design of page.
     return Scaffold(
-
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.pop(context);   // On click, will return to "context" (in this case, home page).
-          }, child: const Text("Your mom"),
-        ),
-      )
-
-    );
+        body: Center(
+      child: ElevatedButton(
+        onPressed: () {
+          Navigator.pop(
+              context); // On click, will return to "context" (in this case, home page).
+        },
+        child: const Text("Your mom"),
+      ),
+    ));
   }
 }
 
@@ -141,18 +143,16 @@ class BunnellPage extends StatelessWidget {
   const BunnellPage({super.key});
 
   @override
-
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: const Text("Your Dad"),
-        ),
-      )
-    );
+        body: Center(
+      child: ElevatedButton(
+        onPressed: () {
+          Navigator.pop(context);
+        },
+        child: const Text("Your Dad"),
+      ),
+    ));
   }
 }
 
@@ -160,65 +160,74 @@ class BunnellPage extends StatelessWidget {
 //           Duckering Building
 //    -------------------------------
 
-
 class DuckPage extends StatelessWidget {
   const DuckPage({super.key});
 
   @override
-
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: const Text ("Your Grandfather"),
-        )
-      )
-    );
+        body: Center(
+            child: ElevatedButton(
+      onPressed: () {
+        Navigator.pop(context);
+      },
+      child: const Text("Your Grandfather"),
+    )));
   }
 }
-
 
 //    -------------------------------
 //              ELIF Building
 //    -------------------------------
 
-
-class ELIFPage extends StatelessWidget {
-  const ELIFPage({super.key});
-
+class ELIFPage extends StatefulWidget {
   @override
+  State<ELIFPage> createState() => _ELIFPageState();
+}
 
-  Widget build(BuildContext context){
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("ELIF Building"),
-        backgroundColor: Colors.deepPurple,
-        centerTitle: true,
-      ),
-      body: Stack(
-        children: [
-          Align(
-            alignment: Alignment.topCenter,
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: const Text("Home"),
-        ),
+class _ELIFPageState extends State<ELIFPage> {
+
+  List<String> floorNumbers = ["Floor 1", "Floor 2", "Floor 3", "Floor 4", "Floor 5"];
+  String selectVal = "Floor 1";
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: const Text("ELIF Building"),
+            backgroundColor: Colors.deepPurple,
+            centerTitle: true,
           ),
-		  /*
-          DropdownButton(
-            items: const [
-              DropdownMenuItem(child: Text("Test"), value: 1),
-              DropdownMenuItem(child: Text("Test2"), value: 2),
+          body: Stack(
+            children: [
+              Align(
+                alignment: Alignment.topCenter,
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: const Text("Home"),
+                ),
+              ),
+               Padding(
+                  padding: const EdgeInsets.only(top:200),
+                   child: DropdownButton(
+                     isExpanded: true,
+                     value: selectVal,
+                     onChanged: (value){
+                       setState(() {
+                         selectVal = value.toString();
+                       });
+                     },
+                     items: floorNumbers.map((itemone){
+                       return DropdownMenuItem(
+                           value: itemone,
+                           child: Text(itemone)
+                       );
+                     }).toList(),
+                  )),
             ],
-            onChanged: (value);
-          ),*/
-       ],
-      )
+          )),
     );
   }
 }
